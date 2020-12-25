fc3839e1698175823601145a873de30a684fb082
Ramarao <bhanud7111@gmail.com>
2020-12-25 13:23:35 +0530

    Rama Project Building

---
+++ src/main/java/scripts/LoginTest.java
@@ -0,0 +1,29 @@
+package scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void loginPageValidScenario()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.bestbuy.ca/");
+		driver.manage().window().maximize();
+		WebElement account = driver.findElement(By.xpath("//span[text()='Account']"));
+		account.click();
+		WebElement username = driver.findElement(By.xpath("//input[@id='username']"));
+		username.sendKeys("ramaraod711@gmail.com");
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("Srirama@711");
+		WebElement signIn = driver.findElement(By.xpath("(//span[text()='Sign In'])[2]/.."));
+		signIn.click();
+		
+		
+	}
+
+}
